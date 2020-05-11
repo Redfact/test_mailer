65e3db2dc630de82ce22bd5172fac9fdf48bb876
Redfact <tantelyrandria@ymail.com>
2020-05-11 17:56:39 +0200

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,15 +60,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   #config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  #config.action_mailer.perform_deliveries = true
 
-  ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_LOGIN'],
-  :password => ENV['SENDGRID_PWD'],
-  :domain => 'no-reply@monsite.fr',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-  }
+
 end
